eaad0e4147b123934f1947cddbb826bec6bbd530
Zakharov Roman, AN-34 <ambeadexter@yandex.ru>
2023-05-18 14:50:18 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# MultimediaGo
+
+Multimedia objects handling implementation
+
+[![Build Status](https://github.com/vorahkaz0namor/MultimediaGo/actions/workflows/gradle.yml/badge.svg)](https://github.com/vorahkaz0namor/MultimediaGo/actions/workflows/gradle.yml)
---
+++ app/src/main/java/ru/sign/conditional/multimediago/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.sign.conditional.multimediago
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
