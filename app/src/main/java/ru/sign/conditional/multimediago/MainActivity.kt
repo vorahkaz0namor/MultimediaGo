0393662f2f8aca7ad1a56dae732790f5fbe069b0
Zakharov Roman, AN-34 <ambeadexter@yandex.ru>
2023-05-19 19:17:02 +0700

    Examples from lecture was implemented

--- app/src/main/java/ru/sign/conditional/multimediago/MainActivity.kt
+++ app/src/main/java/ru/sign/conditional/multimediago/MainActivity.kt
@@ -1,11 +1,82 @@
 package ru.sign.conditional.multimediago
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
+import androidx.annotation.RawRes
+import androidx.annotation.StringRes
+import ru.sign.conditional.multimediago.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val observer = MediaLifecycleObserver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        lifecycle.addObserver(observer)
+        binding.apply {
+            musicPlayButton.setOnClickListener {
+//                setLocalDataSource(R.raw.song)
+                setNetDataSource(R.string.link_to_play)
+            }
+            videoPlayButton.setOnClickListener {
+                setNetVideo(R.string.video_link_to_play)
+            }
+            stopButton.setOnClickListener {
+                observer.mediaPlayer?.stop()
+                videoView.stopPlayback()
+            }
+        }
+    }
 
+    // Передача ЛОКАЛЬНОГО файла в MediaPlayer
+    private fun setLocalDataSource(@RawRes rawId: Int) {
+        observer.apply {
+            // Сначала надо получить доступ к файлу
+            resources.openRawResourceFd(rawId)
+                .use { assetFileDescriptor ->
+                    mediaPlayer?.apply {
+                        observer.mediaPlayerStateCheck()
+                        // Здесь файл передается в MediaPlayer
+                        setDataSource(
+                            assetFileDescriptor.fileDescriptor,
+                            assetFileDescriptor.startOffset,
+                            assetFileDescriptor.length
+                        )
+                    }
+                }
+        }
+            // Запуск воспроизведения
+            .play()
+    }
+
+    // Передача СЕТЕВОГО файла в MediaPlayer
+    private fun setNetDataSource(@StringRes link: Int) {
+        observer.apply {
+            mediaPlayer?.apply {
+                observer.mediaPlayerStateCheck()
+                setDataSource(getString(link))
+            }
+        }
+            .play()
+    }
+
+    // Передача СЕТЕВОГО файла в VideoView
+    private fun setNetVideo(@StringRes link: Int) {
+        binding.videoView.apply {
+            // Медиаконтроллер для управления воспроизведением
+            setMediaController(MediaController(this@MainActivity))
+            setVideoURI(
+                Uri.parse(getString(link))
+            )
+            // Чтобы после подготовки началось воспроизведение,
+            // необходимо установить слушателя
+            setOnPreparedListener { start() }
+            // А чтобы по окончании проирывания плеер закрылся,
+            // необходимо установить вот такого слушателя
+            setOnCompletionListener { stopPlayback() }
+        }
     }
 }
---
+++ app/src/main/java/ru/sign/conditional/multimediago/MediaLifecycleObserver.kt
@@ -0,0 +1,40 @@
+package ru.sign.conditional.multimediago
+
+import android.media.MediaPlayer
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
+
+class MediaLifecycleObserver : LifecycleEventObserver {
+    // Переменная, с помощью которой будет изменяться состояние
+    // MediaPlayer'а в зависимости от жизненного цикла Activity
+    var mediaPlayer: MediaPlayer? = MediaPlayer()
+
+    // Функция, которая будет запускать воспроизведение
+    fun play() {
+        mediaPlayer?.apply {
+            setOnPreparedListener { start() }
+            prepareAsync()
+        }
+    }
+
+    fun mediaPlayerStateCheck() {
+        mediaPlayer?.reset()
+    }
+
+    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+        when (event) {
+            Lifecycle.Event.ON_START ->
+                if (mediaPlayer == null)
+                    mediaPlayer = MediaPlayer()
+            Lifecycle.Event.ON_PAUSE -> mediaPlayer?.pause()
+            Lifecycle.Event.ON_STOP -> {
+                mediaPlayer?.release()
+                mediaPlayer = null
+            }
+            Lifecycle.Event.ON_DESTROY ->
+                source.lifecycle.removeObserver(this)
+            else -> Unit
+        }
+    }
+}
